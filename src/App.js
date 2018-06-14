273f74adc7963ae63c522e695676d7ee102f39a3
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-06-13 20:04:26 -0500

    works

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React from "react";
 import TopNav from "./components/TopNav";
 import SideNav from "./components/SideNav";
+import Dash from "./components/Dashboard";
+import Charts from "./components/AreaChart";
+import Tables from "./components/DonutChart";
+import Profiles from "./components/Profiles";
+import Profile from "./components/Profile";
+import Marquee from "./components/Marquee";
+import Wall from "./components/Wall";
+import Settings from "./components/Settings";
+
+
+import {
+  BrowserRouter,
+  Route,
+  Switch
+ } from "react-router-dom";
+ 
 
 function App() {
   return (
     <div> 
+    <BrowserRouter>
       <div id="wrapper">
         <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
           <TopNav />
           <SideNav />
         </nav>
         <div style={{backgroundColor: "white"}}>
-          {/* PUT YOUR ROUTES HERE */}
+        
+         <div>
+         <Switch>
+            
+            <Route path="/charts" component={Charts} />
+            <Route path="/tables" component={Tables} />
+            <Route path="/settings" component={Settings} />
+            <Route path="/wall" component={Wall} />
+            <Route path="/profiles" component={Profiles} />
+            <Route path="/marquee/:text" component={Marquee} />
+            <Route path="/profile/:id" component={Profile} />
+            <Route path="/" component={Dash} />
+            
+         </Switch>
+         </div>
+       
         </div>
       </div>
+      </BrowserRouter>
     </div>
 
   );
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,26 +1,46 @@
 import React from "react";
-// import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function SideNav() {
   return (
     <div className="collapse navbar-collapse navbar-ex1-collapse">
       <ul className="nav navbar-nav side-nav">
         <li className="active">
-          {/*
+          
           <Link to="/"> <i className="fa fa-fw fa-dashboard" />
             Dashboard 
           </Link>
-          */}
+       
         </li>
         <li>
-          <a href="charts.html">
+          <Link to="/charts"> 
             <i className="fa fa-fw fa-bar-chart-o" /> Charts
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="tables.html">
-            <i className="fa fa-fw fa-table" /> Tables
-          </a>
+
+        <Link to="/tables"> 
+         <i className="fa fa-fw fa-table" /> Tables
+        </Link>
+
+        <Link to="/settings"> 
+         <i className="fa fa-fw fa-table" /> Settings
+        </Link>
+
+        <Link to="/wall"> 
+         <i className="fa fa-fw fa-table" /> Wall
+        </Link>
+
+        <Link to="/profiles"> 
+         <i className="fa fa-fw fa-table" /> Profiles
+        </Link>
+
+        <Link to="/marquee/iloveroutes"> 
+         <i className="fa fa-fw fa-table" />  Marquee
+        </Link>
+
+
+         
         </li>
       </ul>
     </div>);  
